3c6ae3c0930c8a3b998f7ebc78558f2170d7d081
이민규 <aaasssddd123@naver.com>
2022-01-19 13:46:30 +0900

    feat ondo project

---
+++ ondo/src/main/java/com/nextlevel/ondo/OndoApplication.java
@@ -0,0 +1,13 @@
+package com.nextlevel.ondo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndoApplication.class, args);
+	}
+
+}
---
+++ ondo/src/test/java/com/nextlevel/ondo/OndoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextlevel.ondo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
