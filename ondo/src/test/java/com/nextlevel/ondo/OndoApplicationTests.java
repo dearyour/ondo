56a1cf4ffbf65ad1c95a5dc3baf234d391411b29
박현우 <62179139+qweadzs@users.noreply.github.com>
2022-01-26 00:06:00 +0900

    fix: gradlew build & test error on EC2

--- ondo/src/test/java/com/nextlevel/ondo/OndoApplicationTests.java
+++ ondo/src/test/java/com/nextlevel/ondo/OndoApplicationTests.java
@@ -1,9 +1,8 @@
 package com.nextlevel.ondo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class OndoApplicationTests {
 
 	@Test
