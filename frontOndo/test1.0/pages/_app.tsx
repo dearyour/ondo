efbcfab6595b3a6299268fe7c8419f680fc7da5e
김용희 <52556956+dearyour@users.noreply.github.com>
2022-01-26 17:51:49 +0900

    fix
    fix

--- frontOndo/test1.0/pages/_app.tsx
+++ frontOndo/test1.0/pages/_app.tsx
@@ -1,29 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types'
-import 'antd/dist/antd.css'
-import Head from 'next/head';
-// import './index.css';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-//index.tsx의 부모, 모든 pages의 상위 타입
-//page에서 공통적인분은 여기다 적기
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
 
-//index.tsx의 레이아웃컴포넌트가 여기인자로 들어감 
-const App = ({Component} :any) =>{
-    return (
-        <>
-        <Head>
-            <meta charSet="utf-8" />
-            <title>Ondo , 3일간의 도전</title>
-        </Head>
-        <div>공통메뉴</div>
-        <Component />
-
-        </>
-    );
-
-};
-
-App.propTypes  = {
-    Component: PropTypes.elementType.isRequired,
-};
-export default App;
+export default MyApp
