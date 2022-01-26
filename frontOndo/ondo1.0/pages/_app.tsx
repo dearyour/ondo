1de3bb81fc4813026976d6e4cb5df4a3542c7765
김용희 <52556956+dearyour@users.noreply.github.com>
2022-01-26 15:11:01 +0900

    delete ondo1.0
    delete ondo1.0

--- frontOndo/ondo1.0/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { AppProps } from 'next/app';
-import { NextPage } from 'next';
-import { wrapper } from '../store';
-
-const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
-
-export default wrapper.withRedux(MyApp);
