8a576055c28d48067355e2aae17fcb822d17a5ff
Elgcha <vkdlfl681@gmail.com>
2022-02-07 13:49:38 +0900

    remove: test code remove
    Former-commit-id: 2885f822b94dffa1386de2470c1298718c86ad2a

--- frontOndo/test-ondo/next.config.js
+++ frontOndo/test-ondo/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    KAKAO_LOGIN: process.env.KAKAO_LOGIN,
-    BACK_EC2: process.env.BACK_EC2
-  }
 }
 
 const withImages = require('next-images');
