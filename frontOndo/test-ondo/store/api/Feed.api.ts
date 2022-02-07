3ed2bd6051fb36ac34bf1d2975f21d401eac4f42
Elgcha <vkdlfl681@gmail.com>
2022-02-07 13:08:39 +0900

    refactor: add env local and code refactoring
    Former-commit-id: 2c1f4dbcb457872c62edc208ef9c70700f7fef2e

--- frontOndo/test-ondo/store/api/Feed.api.ts
+++ frontOndo/test-ondo/store/api/Feed.api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const Feedurl = "http://i6a601.p.ssafy.io:8080/feed";
-const placeHolderurl = "http://i6a601.p.ssafy.io:8080/feed";
+const Feedurl = process.env.BACK_EC2 + "/feed";
+const placeHolderurl = process.env.BACK_EC2 + "/feed";
 export const GetFeedState = (token: string | null) => {
   return axios({
     method: "GET",
