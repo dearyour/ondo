a5719a83dc1a23ed1fd28c35057d3d3a06786cdc
Elgcha <vkdlfl681@gmail.com>
2022-02-07 11:27:33 +0900

    feat: saga router push
    Former-commit-id: a343496508eb3da4aa5f9649c3b5a9b48240b133

--- frontOndo/test-ondo/store/sagas/User.saga.ts
+++ frontOndo/test-ondo/store/sagas/User.saga.ts
@@ -13,23 +13,24 @@ import { User } from "../interfaces/User.interface";
 import { userActions } from "../slice/user";
 import { KakaoLogin, ProfileEdit, GetUserState } from "../api/User.api";
 import { stringify } from "querystring";
-import { useRouter } from "next/router";
-// import { router } from "react-router-redux";
+import Router from "next/router";
+
+
 
 function* getKakaoKey() {
   interface tokentype extends AxiosResponse {
     token: string;
   }
-  const router: any = useRouter();
   try {
     const code = new URL(window.location.href).searchParams.get("code");
     const response: tokentype = yield call(KakaoLogin, code);
     console.log(response.token);
     console.log(code);
     yield put(userActions.getKakaoKeySuccess(response.token));
-    yield put(router.push("/"));
+    Router.push('/')
   } catch (err) {
     yield put(userActions.getKakaoKeyError(err));
+    Router.push('/')
   }
 }
 
