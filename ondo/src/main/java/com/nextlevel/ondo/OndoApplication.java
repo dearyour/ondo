185f1d8ce782511a510b11ace908dee70d609afa
박현우 <62179139+qweadzs@users.noreply.github.com>
2022-02-11 00:55:54 +0900

    fix: jenkins application running error
    Former-commit-id: e77f42859eb9a084381e926fb569c9c0fe14722d

--- ondo/src/main/java/com/nextlevel/ondo/OndoApplication.java
+++ ondo/src/main/java/com/nextlevel/ondo/OndoApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableWebMvc
 public class OndoApplication {
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.properties,"
