0b67f0bb590567d10ef56eef55bee07ca98c81ae
Elgcha <vkdlfl681@gmail.com>
2022-02-12 19:54:47 +0900

    bug: 빌드 시 에러나는 것 수정
    Former-commit-id: bf227d13731beec91d1316616c05fd8345a0bcae

--- frontOndo/test-ondo/components/challenge/CategoryIcons.tsx
+++ frontOndo/test-ondo/components/challenge/CategoryIcons.tsx
@@ -12,7 +12,7 @@ import etc from 'public/images/category/etc.png';
 import Image from "next/image";
 import axios from "axios";
 
-const CategoryIcons = (props : any) => {
+const CategoryIcons = (props: any) => {
   const categoryIcons = [overall, exercise, eating, hobbies, learning, eco, appearance, etc];
   const categoryLabels = ['전체', '운동', '식습관', '취미', '학습', '친환경', '외모관리', '기타'];
   const categoryPath = ['/exercise', '/eating', '/hobbies', '/learning', '/eco', '/appearance', '/etc'];
@@ -38,7 +38,7 @@ const CategoryIcons = (props : any) => {
         categoryIcons.map((v, i) => (
           <ColCenter xs={3} md={3} key={i}>
             <Space direction="vertical">
-              <Image src={v} width={50} height={50} name={categoryLabels[i]} onClick={() => props.changeCategory(categoryLabels[i])}></Image>
+              <Image src={v} width={50} height={50} alt={categoryLabels[i]} onClick={() => props.changeCategory(categoryLabels[i])}></Image>
               <LabelCenter htmlFor={categoryLabels[i]}>{categoryLabels[i]}</LabelCenter>
             </Space>
           </ColCenter>
--- frontOndo/test-ondo/components/layout/AppLayout.tsx
+++ frontOndo/test-ondo/components/layout/AppLayout.tsx
@@ -42,7 +42,6 @@ const AppLayout: React.FC<LayoutProps> = ({ children, title = '' }: LayoutProps)
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
     </Head>
     {/* <Layout> */}
     {/* <Header> */}
