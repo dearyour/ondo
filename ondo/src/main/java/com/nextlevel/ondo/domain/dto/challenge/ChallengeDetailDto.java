9d4df43bb667997df413d699aaeaa61835f338fa
박현우 <62179139+qweadzs@users.noreply.github.com>
2022-02-13 15:52:09 +0900

    feat: isStarted
    Former-commit-id: 615e521a440e6f1bd2cda28147287476088241bc

--- ondo/src/main/java/com/nextlevel/ondo/domain/dto/challenge/ChallengeDetailDto.java
+++ ondo/src/main/java/com/nextlevel/ondo/domain/dto/challenge/ChallengeDetailDto.java
@@ -16,6 +16,7 @@ public class ChallengeDetailDto {
     // 해당 DTO는 챌린지, 여부 등을 담는 객체.
     private boolean isFinished; // 끝난 챌린지인지 여부
     private boolean amIParticipate; // 내가 참여 중인지 여부
+    private boolean isStarted;
     private Challenge challenge;
     private List<Feed> feeds; // 해당 챌린지 관련 피드 리스트
 
